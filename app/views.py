6e131dd898bb3353ebed2d711619e68d65ec7718
Arman <119151658+arman721@users.noreply.github.com>
2024-10-02 00:33:26 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import redirect, render
+
+from app.models import *
+
+
+def home(request):
+    return render(request,'app/template/home.html')
+def student_reg(request):
+    if request.method=='POST':
+        data=request.POST
+        
+        name=data.get("student_name")
+        age=data.get("age")
+        address=data.get("address")
+        
+        student.objects.create(name=name,age=age,address=address)
+        
+        
+    students=student.objects.all()
+    context={'students':students}
+    return render(request,'app/template/register.html',context)
+    
+
+# Create your views here.
